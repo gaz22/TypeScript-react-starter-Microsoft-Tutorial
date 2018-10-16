cec59ba9d213cc5f922f2a3b2a41eea0dddc88de
Gazza Azhari <gaz22.dev@gmail.com>
2018-10-17 09:00:57 +1100

    Adding redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction } from './actions/index';
+import { StoreState } from './types/index';
+import { enthusiasm } from './reducers/index';
+
+
+const store = createStore<StoreState, EnthusiasmAction, null, null>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript'
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
